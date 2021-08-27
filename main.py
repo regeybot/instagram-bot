13d672b3e545c79481b85e6c95ccc1790a5f7705
regeybot <89459508+regeybot@users.noreply.github.com>
2021-08-28 01:01:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1955443902:AAF_XFJrpFZJ4-8twtG18a_VS_uSrgnNnJ4"
 
 def download(bot, update):
     message = update.effective_message
